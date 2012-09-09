ce38f71dfc9dbb074cfbe99b6c8515ee97374e91
Fabio <fabio@fabio-Infoway.(none)>
2012-09-09 12:29:10 +0000

    conserto em itemcaixa

---
+++ app/models/FonecedorRepresentanteRecord.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ *@Caique Pires
+ */
+
+class FornecedorRepresentanteRecord extends Manipulabanco{
+    
+    public function cadastrar($dados){
+    return $this->salvar($dados);
+  }
+    
+}
+
+
+?>
